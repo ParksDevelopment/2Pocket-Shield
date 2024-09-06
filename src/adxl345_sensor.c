0bd895b4c8aa9d5f746300c1b34f9c426bb423ea
Jcam599 <parksDevelop@gmail.com>
2024-09-06 16:42:09 -0400

    trying to find the device

--- src/adxl345_sensor.c
+++ src/adxl345_sensor.c
@@ -7,7 +7,7 @@
 LOG_MODULE_REGISTER(adxl345);
 
 static int adxl345_init(const struct device *dev) {
-    const struct device *sensor = device_get_binding("ADXL345");
+    const struct device *sensor = device_dt_get("adxl345");
 
     if (!sensor) {
         LOG_ERR("No device found for ADXL345");
