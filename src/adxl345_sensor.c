ac0cbc2487f36d757394f9e3eaba0347ffbea892
Jcam599 <parksDevelop@gmail.com>
2024-09-13 11:34:12 -0400

    trying to remove sample testing

--- src/adxl345_sensor.c
+++ src/adxl345_sensor.c
@@ -18,29 +18,29 @@ static void adxl345_read_data(const struct device *sensor) {
     struct sensor_value accel_x, accel_y, accel_z;
     int ret;
 
-    ret = sensor_sample_fetch(sensor);
-    if (ret) {
-        LOG_ERR("Failed to fetch sample from ADXL345: %d", ret);
-        return;
-    }
+    // ret = sensor_sample_fetch(sensor);
+    // if (ret) {
+    //     LOG_ERR("Failed to fetch sample from ADXL345: %d", ret);
+    //     return;
+    // }
 
-    ret = sensor_channel_get(sensor, SENSOR_CHAN_ACCEL_X, &accel_x);
-    if (ret) {
-        LOG_ERR("Failed to get X-axis data: %d", ret);
-        return;
-    }
+    // ret = sensor_channel_get(sensor, SENSOR_CHAN_ACCEL_X, &accel_x);
+    // if (ret) {
+    //     LOG_ERR("Failed to get X-axis data: %d", ret);
+    //     return;
+    // }
 
-    ret = sensor_channel_get(sensor, SENSOR_CHAN_ACCEL_Y, &accel_y);
-    if (ret) {
-        LOG_ERR("Failed to get Y-axis data: %d", ret);
-        return;
-    }
+    // ret = sensor_channel_get(sensor, SENSOR_CHAN_ACCEL_Y, &accel_y);
+    // if (ret) {
+    //     LOG_ERR("Failed to get Y-axis data: %d", ret);
+    //     return;
+    // }
 
-    ret = sensor_channel_get(sensor, SENSOR_CHAN_ACCEL_Z, &accel_z);
-    if (ret) {
-        LOG_ERR("Failed to get Z-axis data: %d", ret);
-        return;
-    }
+    // ret = sensor_channel_get(sensor, SENSOR_CHAN_ACCEL_Z, &accel_z);
+    // if (ret) {
+    //     LOG_ERR("Failed to get Z-axis data: %d", ret);
+    //     return;
+    // }
 
     LOG_INF("ADXL345 Acceleration: X: %d.%06d, Y: %d.%06d, Z: %d.%06d",
             accel_x.val1, accel_x.val2,
@@ -50,11 +50,7 @@ static void adxl345_read_data(const struct device *sensor) {
 
 static void adxl345_thread(void) {
     while (1) {
-        int i;
-
-        for (i = 0; i < 20; i++) {
-            adxl345_read_data(sensor);
-        }
+        adxl345_read_data(sensor);
         k_sleep(READ_INTERVAL);
     }
 }
